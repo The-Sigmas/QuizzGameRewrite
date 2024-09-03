f1f7531a1f757038b28e38f1756b216ffb0af638
Zer0 <max.hasselwander@wiss-edu.ch>
2024-09-03 16:44:47 +0200

    added the test files needed to run tests also finished the layout test file

--- src/components/Layout.test.jsx
+++ src/components/Layout.test.jsx
@@ -1,5 +1,35 @@
+
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, screen, waitFor } from '@testing-library/react';
 import Layout from './Layout';
-import Category from './Category';
+import '@testing-library/jest-dom';
+
+
+jest.mock('./Category', () => (props) => (
+  <button className={props.s} data-testid={`category-${props.name}`}>
+    {props.name}
+  </button>
+));
+
+describe('Layout Component', () => {
+  test('renders category buttons', async () => {
+    render(<Layout />);
+
+    await waitFor(() => {
+      const categoryButtons = screen.getAllByRole('button');
+      expect(categoryButtons).toHaveLength(3);
+    });
+    expect(screen.getByTestId('category-autos')).toHaveTextContent('autos');
+    expect(screen.getByTestId('category-cpu')).toHaveTextContent('cpu');
+    expect(screen.getByTestId('category-gpu')).toHaveTextContent('gpu');
+  });
+
+  test('renders the Outlet component', async () => {
+    render(<Layout />);
+
+    await waitFor(() => {
+      const outlet = screen.queryByTestId('outlet');
+      expect(outlet).toBeInTheDocument();
+    });
+  });
+});
