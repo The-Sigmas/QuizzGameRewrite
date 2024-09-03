45a608615f650ca1d04d64c7675de795cfdcbe25
bomba-cat <bledion.morina@hotmail.com>
2024-09-03 14:28:37 +0200

    silly

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export default function Quiz( parts ) {
+  const questions={
+    id: 1,
+    question: { parts.q1 },
+    options: { parts.o11, parts.o12, parts.o13 },
+    answer: { parts.a1 },
+  }
+  return(<>
+
+  </>)
+}
